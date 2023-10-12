54a4e962b1cc3e2e7e52a0bf78768eff61ab7a28
malken21 <Malken_Marumasa@outlook.jp>
2023-10-13 08:33:06 +0900

    Create VehicleController.java

---
+++ src/main/java/marumasa/marumasa_car/VehicleController.java
@@ -0,0 +1,11 @@
+package marumasa.marumasa_car;
+
+import marumasa.marumasa_car.vehicle.Vehicle;
+import org.bukkit.entity.ArmorStand;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class VehicleController {
+    public static Map<ArmorStand, Vehicle> VehicleLink = new HashMap<>();
+}
