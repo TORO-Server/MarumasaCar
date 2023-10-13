2b2e66090c45194b14e173ccf8fd896e1cd8d628
malken21 <Malken_Marumasa@outlook.jp>
2023-10-13 14:22:35 +0900

    Update ExampleCar.java

--- src/main/java/marumasa/marumasa_car/vehicle/machine/ExampleCar.java
+++ src/main/java/marumasa/marumasa_car/vehicle/machine/ExampleCar.java
@@ -1,6 +1,11 @@
 package marumasa.marumasa_car.vehicle.machine;
 
 import marumasa.marumasa_car.vehicle.Vehicle;
+import marumasa.marumasa_car.vehicle.VehicleUtils;
+import marumasa.marumasa_car.vehicle.parts.Body;
+import marumasa.marumasa_car.vehicle.parts.MainSeat;
+import marumasa.marumasa_car.vehicle.parts.Seat;
+import org.bukkit.Material;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -8,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import marumasa.marumasa_car.vehicle.parts.Part;
+import org.bukkit.util.Vector;
 
 public class ExampleCar extends Vehicle {
     public static final String tag = "marumasa.example";
@@ -19,6 +25,19 @@ public class ExampleCar extends Vehicle {
     @Override
     public List<Part> partsList() {
         return new ArrayList<>() {{
+            add(new Seat(new Vector(0.5, 0.5, 0.5)));
+            add(new Seat(new Vector(0.5, 0.5, -0.5)));
+            add(new Seat(new Vector(-0.5, 0.5, -0.5)));
+            add(new Body(new Vector(0, 0.5, 0), Material.DISC_FRAGMENT_5));
+            add(new MainSeat(new Vector(-0.5, 0.5, 0.5)));
         }};
+    }
+
+    @Override
+    public void remove() {
+
+        dropVehicleItem(tag);
+
+        super.remove();
     }
 }
