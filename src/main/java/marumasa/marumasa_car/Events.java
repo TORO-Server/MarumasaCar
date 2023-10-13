5240d15479c4a51d24c1fbd45782a88a591e5096
malken21 <Malken_Marumasa@outlook.jp>
2023-10-13 10:12:29 +0900

    update events

--- src/main/java/marumasa/marumasa_car/Events.java
+++ src/main/java/marumasa/marumasa_car/Events.java
@@ -1,15 +1,7 @@
 package marumasa.marumasa_car;
 
-import com.comphenix.protocol.PacketType;
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.events.ListenerPriority;
-import com.comphenix.protocol.events.PacketAdapter;
-import com.comphenix.protocol.events.PacketContainer;
-import com.comphenix.protocol.events.PacketEvent;
-import marumasa.marumasa_car.vehicle.machine.TestCar;
-import marumasa.marumasa_car.vehicle.Vehicle;
-import marumasa.marumasa_car.vehicle.VehicleController;
-import org.bukkit.entity.*;
+import marumasa.marumasa_car.vehicle.VehicleEventManager;
+import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
@@ -17,103 +9,40 @@ import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.world.EntitiesLoadEvent;
 import org.bukkit.event.world.EntitiesUnloadEvent;
-
-import java.util.Set;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class Events implements Listener {
 
     private final Config cfg;
-    private final Minecraft mc;
+    private final VehicleEventManager eventManager;
 
-    public Events(Config config, Minecraft minecraft) {
+    public Events(Config config, JavaPlugin plugin) {
         cfg = config;
-        mc = minecraft;
-
-        ProtocolLibrary.getProtocolManager().addPacketListener(new PacketAdapter(mc, ListenerPriority.HIGHEST, PacketType.Play.Client.STEER_VEHICLE) {
-            @Override
-            public void onPacketReceiving(PacketEvent event) {
-                PacketContainer packet = event.getPacket();
-                Player player = event.getPlayer();
-
-                Entity b = player.getVehicle();
-                if (b instanceof Player) {
-                    return;
-                }
-
-                if (packet.getFloat().read(1) > 0) {
-                    VehicleController.W.add(player);
-                } else if (packet.getFloat().read(1) < 0) {
-                    VehicleController.S.add(player);
-                } else {
-                    VehicleController.W.remove(player);
-                    VehicleController.S.remove(player);
-                }
-
-                if (packet.getFloat().read(0) > 0) {
-                    VehicleController.A.add(player);
-                } else if (packet.getFloat().read(0) < 0) {
-                    VehicleController.D.add(player);
-                } else {
-                    VehicleController.A.remove(player);
-                    VehicleController.D.remove(player);
-                }
-
-                if (packet.getBooleans().read(0)) {
-                    VehicleController.Jump.add(player);
-                } else {
-                    VehicleController.Jump.remove(player);
-                }
-            }
-        });
+        eventManager = new VehicleEventManager(plugin);
     }
 
     @EventHandler
     private void onEntitySpawn(EntitySpawnEvent event) {
-        Load(event.getEntity());
+        eventManager.load(event.getEntity());
     }
 
     @EventHandler
     private void onEntityDeath(EntityDeathEvent event) {
-        Unload(event.getEntity());
+        eventManager.unload(event.getEntity());
     }
 
     @EventHandler
     private void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
-        Ride(event.getRightClicked(), event.getPlayer());
+        eventManager.ride(event.getRightClicked(), event.getPlayer());
     }
 
     @EventHandler
     private void onEntityLoad(EntitiesLoadEvent event) {
-        for (Entity entity : event.getEntities())
-            Load(entity);
+        for (Entity entity : event.getEntities()) eventManager.load(entity);
     }
 
     @EventHandler
     private void onEntitiesUnload(EntitiesUnloadEvent event) {
-        for (Entity entity : event.getEntities())
-            Unload(entity);
-    }
-
-    private void Ride(Entity entity, Player player) {
-        if (entity instanceof Interaction interaction) {
-            if (interaction.getPassengers().size() != 0 && !VehicleController.SeatList.contains(interaction)) return;
-            interaction.addPassenger(player);
-        }
-    }
-
-    private void Load(Entity entity) {
-        final Set<String> tags = entity.getScoreboardTags();
-        if (entity instanceof ArmorStand stand) {
-            if (tags.contains("marumasa.test")) {
-               new  TestCar(stand, mc);
-            }
-        }
-    }
-
-    private void Unload(Entity entity) {
-        if (entity instanceof ArmorStand stand) {
-            Vehicle vehicle = VehicleController.VehicleLink.get(stand);
-            if (vehicle != null) vehicle.remove();
-        }
+        for (Entity entity : event.getEntities()) eventManager.unload(entity);
     }
 }
--- src/main/java/marumasa/marumasa_car/vehicle/VehicleEventManager.java
+++ src/main/java/marumasa/marumasa_car/vehicle/VehicleEventManager.java
@@ -1,4 +1,84 @@
 package marumasa.marumasa_car.vehicle;
 
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.events.ListenerPriority;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.events.PacketEvent;
+import marumasa.marumasa_car.vehicle.machine.TestCar;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Interaction;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Set;
+
 public class VehicleEventManager {
+    private final JavaPlugin pl;
+
+    public VehicleEventManager(JavaPlugin plugin) {
+        pl = plugin;
+
+        ProtocolLibrary.getProtocolManager().addPacketListener(new PacketAdapter(pl, ListenerPriority.HIGHEST, PacketType.Play.Client.STEER_VEHICLE) {
+            @Override
+            public void onPacketReceiving(PacketEvent event) {
+                PacketContainer packet = event.getPacket();
+                Player player = event.getPlayer();
+
+                Entity b = player.getVehicle();
+                if (b instanceof Player) {
+                    return;
+                }
+
+                if (packet.getFloat().read(1) > 0) {
+                    VehicleController.W.add(player);
+                } else if (packet.getFloat().read(1) < 0) {
+                    VehicleController.S.add(player);
+                } else {
+                    VehicleController.W.remove(player);
+                    VehicleController.S.remove(player);
+                }
+
+                if (packet.getFloat().read(0) > 0) {
+                    VehicleController.A.add(player);
+                } else if (packet.getFloat().read(0) < 0) {
+                    VehicleController.D.add(player);
+                } else {
+                    VehicleController.A.remove(player);
+                    VehicleController.D.remove(player);
+                }
+
+                if (packet.getBooleans().read(0)) {
+                    VehicleController.Jump.add(player);
+                } else {
+                    VehicleController.Jump.remove(player);
+                }
+            }
+        });
+    }
+
+    public void load(Entity entity) {
+        final Set<String> tags = entity.getScoreboardTags();
+        if (entity instanceof ArmorStand stand) {
+            if (tags.contains("marumasa.test")) {
+                new TestCar(stand, pl);
+            }
+        }
+    }
+
+    public void unload(Entity entity) {
+        if (entity instanceof ArmorStand stand) {
+            Vehicle vehicle = VehicleController.VehicleLink.get(stand);
+            if (vehicle != null) vehicle.remove();
+        }
+    }
+
+    public void ride(Entity entity, Player player) {
+        if (entity instanceof Interaction interaction) {
+            if (interaction.getPassengers().size() != 0 && !VehicleController.SeatList.contains(interaction)) return;
+            interaction.addPassenger(player);
+        }
+    }
 }
