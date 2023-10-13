0bfb317734df827f417a056505452d7474ca63c3
malken21 <Malken_Marumasa@outlook.jp>
2023-10-13 13:45:46 +0900

    Update Config.java

--- src/main/java/marumasa/marumasa_car/Config.java
+++ src/main/java/marumasa/marumasa_car/Config.java
@@ -3,7 +3,6 @@ package marumasa.marumasa_car;
 import org.bukkit.configuration.file.FileConfiguration;
 
 public class Config {
-
     public Config(final Minecraft plugin) {
         plugin.saveDefaultConfig();
         FileConfiguration config = plugin.getConfig();
