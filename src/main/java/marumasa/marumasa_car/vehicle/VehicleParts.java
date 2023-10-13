4feff997fec570f907fbef6db2672074b8f61d2c
malken21 <Malken_Marumasa@outlook.jp>
2023-10-13 11:03:51 +0900

    Create VehicleParts.java

---
+++ src/main/java/marumasa/marumasa_car/vehicle/VehicleParts.java
@@ -0,0 +1,57 @@
+package marumasa.marumasa_car.vehicle;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Interaction;
+import org.bukkit.entity.ItemDisplay;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Vector;
+
+import java.util.List;
+
+public class VehicleParts {
+    public static class superPart {
+        public final Vector vector;
+        public final EntityType entityType;
+
+        public final boolean isSeat;
+
+        public superPart(Vector vector, EntityType entityType, boolean isSeat) {
+            this.vector = vector;
+            this.entityType = entityType;
+            this.isSeat = isSeat;
+        }
+
+        public superPart(Vector vector, EntityType entityType) {
+            this(vector, entityType, false);
+        }
+
+        public superPart(Vector vector, boolean isSeat) {
+            this.vector = vector;
+            this.entityType = EntityType.ITEM_DISPLAY;
+            this.isSeat = isSeat;
+        }
+
+        public void create(World world, Location location, List<Entity> trackingEntities) {
+
+            Entity entity = world.spawnEntity(location, entityType);
+
+            trackingEntities.add(entity);
+
+            if (entity instanceof ItemDisplay display) {
+                if (isSeat) {
+                    display.setTeleportDuration(2);
+                    Interaction interaction = (Interaction) world.spawnEntity(location, EntityType.INTERACTION);
+                    display.addPassenger(interaction);
+                    VehicleController.SeatList.add(interaction);
+                } else {
+                    display.setTeleportDuration(2);
+                    display.setItemStack(new ItemStack(Material.STONE));
+                }
+            }
+        }
+    }
+}
