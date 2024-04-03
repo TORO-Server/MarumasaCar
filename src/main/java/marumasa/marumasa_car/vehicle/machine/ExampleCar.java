63c98ac852345956b308ea923acd97823fb83b0c
malken21 <77374813+malken21@users.noreply.github.com>
2024-04-03 18:56:43 +0900

    test

--- src/main/java/marumasa/marumasa_car/vehicle/machine/ExampleCar.java
+++ src/main/java/marumasa/marumasa_car/vehicle/machine/ExampleCar.java
@@ -27,12 +27,161 @@ public class ExampleCar extends Vehicle {
             add(new Seat(new Vector(0.48, 0.5, -0.7), 0.8f, 1));
             add(new Seat(new Vector(-0.48, 0.5, -0.7), 0.8f, 1));
             add(new Seat(new Vector(-0.48, 0.5, 0.45), 0.8f, 1));
+
+
             add(new Body(
-                    Material.DISC_FRAGMENT_5,
-                    new Vector3f(0f, 0f, 0.65f),
-                    new Vector3f(2.6f),
-                    1
+                    Material.QUARTZ_SLAB,
+                    new Vector3f(0f, 0.5f, 3f),
+                    new Vector3f(0f, 0f, 0f),
+                    new Vector3f(1f)
             ));
+            add(new Body(
+                    Material.GRAY_WOOL,
+                    new Vector3f(0f, 0f, 2f),
+                    new Vector3f(0f, 0f, 0f),
+                    new Vector3f(1f)
+            ));
+            add(new Body(
+                    Material.GRAY_WOOL,
+                    new Vector3f(0f, 0f, 1f),
+                    new Vector3f(0f, 0f, 0f),
+                    new Vector3f(1f)
+            ));
+            add(new Body(
+                    Material.GRAY_WOOL,
+                    new Vector3f(0f, 0f, 0f),
+                    new Vector3f(0f, 0f, 0f),
+                    new Vector3f(1f)
+            ));
+            add(new Body(
+                    Material.GRAY_WOOL,
+                    new Vector3f(0f, 0f, -1f),
+                    new Vector3f(0f, 0f, 0f),
+                    new Vector3f(1f)
+            ));
+            add(new Body(
+                    Material.GRAY_WOOL,
+                    new Vector3f(0f, 0f, -2f),
+                    new Vector3f(0f, 0f, 0f),
+                    new Vector3f(1f)
+            ));
+            add(new Body(
+                    Material.GRAY_WOOL,
+                    new Vector3f(0f, 0f, -3f),
+                    new Vector3f(0f, 0f, 0f),
+                    new Vector3f(1f)
+            ));
+            add(new Body(
+                    Material.POLISHED_DEEPSLATE_SLAB,
+                    new Vector3f(0f, 0.5f, -4f),
+                    new Vector3f(0f, 0f, 0f),
+                    new Vector3f(1f)
+            ));
+
+
+
+
+            add(new Body(
+                    Material.POLISHED_DEEPSLATE_STAIRS,
+                    new Vector3f(1f, 0.5f, 3f),
+                    new Vector3f(90f, -90f, 0f),
+                    new Vector3f(1f)
+            ));
+            add(new Body(
+                    Material.COAL_BLOCK,
+                    new Vector3f(1f, 0f, 2f),
+                    new Vector3f(0f, 0f, 0f),
+                    new Vector3f(1f)
+            ));
+            add(new Body(
+                    Material.QUARTZ_BLOCK,
+                    new Vector3f(1f, 0.25f, 1f),
+                    new Vector3f(0f, 0f, 0f),
+                    new Vector3f(0.5f)
+            ));
+            add(new Body(
+                    Material.DARK_OAK_FENCE,
+                    new Vector3f(1f, 0f, 0f),
+                    new Vector3f(0f, 0f, 0f),
+                    new Vector3f(1f)
+            ));
+            add(new Body(
+                    Material.DARK_OAK_FENCE,
+                    new Vector3f(1f, 0f, -1f),
+                    new Vector3f(0f, 0f, 0f),
+                    new Vector3f(1f)
+            ));
+            add(new Body(
+                    Material.POLISHED_DEEPSLATE_STAIRS,
+                    new Vector3f(1f, 0.5f, -2f),
+                    new Vector3f(0f, 0f, 0f),
+                    new Vector3f(1f)
+            ));
+            add(new Body(
+                    Material.COAL_BLOCK,
+                    new Vector3f(1f, 0f, -3f),
+                    new Vector3f(0f, 0f, 0f),
+                    new Vector3f(1f)
+            ));
+            add(new Body(
+                    Material.IRON_TRAPDOOR,
+                    new Vector3f(1f, 0.8f, -4f),
+                    new Vector3f(0f, 0f, 0f),
+                    new Vector3f(1f)
+            ));
+
+
+
+            add(new Body(
+                    Material.QUARTZ_SLAB,
+                    new Vector3f(0f, 0.5f, 3f),
+                    new Vector3f(0f, 0f, 0f),
+                    new Vector3f(1f)
+            ));
+            add(new Body(
+                    Material.GRAY_WOOL,
+                    new Vector3f(0f, 0f, 2f),
+                    new Vector3f(0f, 0f, 0f),
+                    new Vector3f(1f)
+            ));
+            add(new Body(
+                    Material.GRAY_WOOL,
+                    new Vector3f(0f, 0f, 1f),
+                    new Vector3f(0f, 0f, 0f),
+                    new Vector3f(1f)
+            ));
+            add(new Body(
+                    Material.GRAY_WOOL,
+                    new Vector3f(0f, 0f, 0f),
+                    new Vector3f(0f, 0f, 0f),
+                    new Vector3f(1f)
+            ));
+            add(new Body(
+                    Material.GRAY_WOOL,
+                    new Vector3f(0f, 0f, -1f),
+                    new Vector3f(0f, 0f, 0f),
+                    new Vector3f(1f)
+            ));
+            add(new Body(
+                    Material.GRAY_WOOL,
+                    new Vector3f(0f, 0f, -2f),
+                    new Vector3f(0f, 0f, 0f),
+                    new Vector3f(1f)
+            ));
+            add(new Body(
+                    Material.GRAY_WOOL,
+                    new Vector3f(0f, 0f, -3f),
+                    new Vector3f(0f, 0f, 0f),
+                    new Vector3f(1f)
+            ));
+            add(new Body(
+                    Material.POLISHED_DEEPSLATE_SLAB,
+                    new Vector3f(0f, 0.5f, -4f),
+                    new Vector3f(0f, 0f, 0f),
+                    new Vector3f(1f)
+            ));
+
+
         }};
     }
 }
--- src/main/java/marumasa/marumasa_car/vehicle/parts/Body.java
+++ src/main/java/marumasa/marumasa_car/vehicle/parts/Body.java
@@ -4,13 +4,13 @@ import marumasa.marumasa_car.vehicle.Vehicle;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
-import org.bukkit.entity.Display;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.ItemDisplay;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.util.Transformation;
 import org.bukkit.util.Vector;
+import org.joml.Quaternionf;
 import org.joml.Vector3f;
 
 public class Body extends TrackingDisplay {
@@ -20,39 +20,47 @@ public class Body extends TrackingDisplay {
     public final Integer customModelData;
 
     public final Vector3f translation;
+    public final Quaternionf rotation;
     public final Vector3f scale;
 
-    public Body(Material material, Vector3f translation, Vector3f scale, Integer customModelData) {
-        this(new Vector(0f, 0.7f, 0f), material, translation.add(0f, scale.y / 2 - 0.7f, 0f), scale, customModelData);
+    public Body(Material material, Vector3f translation, Quaternionf rotation, Vector3f scale, Integer customModelData) {
+        this(new Vector(0f, 0.7f, 0f), material, translation.add(0f, scale.y / 2 - 0.7f, 0f), rotation, scale, customModelData);
     }
 
-    public Body(Vector vector, Material material, Vector3f translation, Vector3f scale, Integer customModelData) {
+    public Body(Material material, Vector3f translation, Vector3f rotation, Vector3f scale) {
+        this(
+                new Vector(0f, 0.7f, 0f),
+                material,
+                translation.add(0f, scale.y / 2 - 0.7f, 0f),
+                new Quaternionf().rotationX(rotation.x).rotationY(rotation.y).rotationZ(rotation.z),
+                scale
+        );
+    }
+
+    public Body(Vector vector, Material material, Vector3f translation, Quaternionf rotation, Vector3f scale) {
+        this(vector, material, translation, rotation, scale, null);
+    }
+
+    public Body(Vector vector, Material material, Vector3f translation, Quaternionf rotation, Vector3f scale, Integer customModelData) {
         super(vector, EntityType.ITEM_DISPLAY);
         this.material = material;
 
         this.translation = translation;
+        this.rotation = rotation;
         this.scale = scale;
 
         this.customModelData = customModelData;
     }
 
-    public Transformation generateTransformation(Display display) {
-        final Transformation tra = display.getTransformation();
-        return new Transformation(
-                translation,
-                tra.getLeftRotation(),
-                scale,
-                tra.getRightRotation()
-        );
+    public Transformation generateTransformation() {
+        return new Transformation(translation, this.rotation, scale,new Quaternionf());
     }
 
     @Override
     public ItemDisplay create(World world, Location location, Vehicle vehicle) {
         final ItemDisplay itemDisplay = (ItemDisplay) super.create(world, location, vehicle);
 
-        itemDisplay.setTransformation(
-                generateTransformation(itemDisplay)
-        );
+        itemDisplay.setTransformation(generateTransformation());
 
         final ItemStack itemStack = new ItemStack(material);
         final ItemMeta itemMeta = itemStack.getItemMeta();
