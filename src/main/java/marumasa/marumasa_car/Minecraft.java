c761ce586634f649b37ff4d4e3248eece16f1c30
PizzaHarumaki <78fhruby78@gmail.com>
2024-07-27 11:14:51 +0900

    add truck

--- src/main/java/marumasa/marumasa_car/Minecraft.java
+++ src/main/java/marumasa/marumasa_car/Minecraft.java
@@ -2,6 +2,7 @@ package marumasa.marumasa_car;
 
 import marumasa.marumasa_car.vehicle.machine.MinumaCar;
 import marumasa.marumasa_car.vehicle.Machine;
+import marumasa.marumasa_car.vehicle.machine.SimpleTruck;
 import org.bukkit.Server;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -16,6 +17,7 @@ public final class Minecraft extends JavaPlugin {
 
 
         Machine.register("marumasa.minuma", MinumaCar::new);
+        Machine.register("fhrk.simpletruck", SimpleTruck::new);
     }
 
     @Override
